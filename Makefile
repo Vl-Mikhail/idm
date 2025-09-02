91fcd9e073280aeb0fe75838e993a5526a593eaa
miguel <evil_bear@mail.ru>
2025-09-02 17:10:58 +1000

    feat: Обновил Makefile

--- Makefile
+++ Makefile
@@ -1,14 +1,21 @@
 .DEFAULT := build
-.PHONY: fmt vet build
+
+.PHONY: fmt vet build test coverage initdb
 
 test:
 	go test ./...
 
-fmt: test
+coverage: test
+	go test -cover ./...
+
+fmt: coverage
 	go fmt ./...
 
 vet: fmt
 	go vet ./...
+
+initdb:
+	goose up -dir ./migrations
 
 build: vet
 	go build
--- test/database_test.go
+++ test/database_test.go
@@ -31,7 +31,7 @@ func TestGetConfig_EnvVars(t *testing.T) {
 	a.NotEmpty(cfg.Dsn)
 
 	a.Equal("postgres", cfg.DbDriverName)
-	a.Equal("host=127.0.0.1 port=5432 user=postgres password=postgres dbname=postgres sslmode=disable", cfg.Dsn)
+	a.Equal("host=127.0.0.1 port=5432 user=postgres password=postgres dbname=idm_tests sslmode=disable", cfg.Dsn)
 }
 
 func TestDatabase_ConnectDbFailed(t *testing.T) {
